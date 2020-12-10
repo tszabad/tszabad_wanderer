986e2ce198277269bb862aafc086c42ee60b0fb3
Tamas Szabados <tszabad@gmail.com>
2020-12-10 13:01:16 +0100

    the screen is filled with floor tiles

--- box.py
+++ box.py
@@ -1,13 +1,35 @@
 from tkinter import *
 
-class Box(object):
+class Game():
+    def __init__(self):
+        self.tiles = []
+
+    def draw(self, canvas):
+        canvas.create_rectangle(0, 0, 600, 600)
+        for tile in self.tiles:
+            canvas.create_image(tile.testBoxX, tile.testBoxY, anchor=NW, image = tile.image)
+    
+    def add_tiles(self, tiles):
+        self.tiles.append(tiles)
+        return self.tiles
+    
+
+class Tile():
     def __init__(self, testBoxX, testBoxY,image):
         self.testBoxX = testBoxX
         self.testBoxY = testBoxY
         self.image = image
 
-    def draw(self, canvas):
-        canvas.create_rectangle(0, 0, 600, 600)
-        
-        canvas.create_image(self.testBoxX, self.testBoxY, anchor=NW, image = self.image)
-        
+class Floor(Tile):
+    def __init__(self, testBoxX, testBoxY,image):
+        super().__init__(testBoxX, testBoxY,image)
+
+class Wall(Tile):
+    def __init__(self, testBoxX, testBoxY,image):
+        super().__init__(testBoxX, testBoxY,image)
+
+
+   
+
+    
+    
--- main.py
+++ main.py
@@ -1,36 +1,51 @@
 from tkinter import *
 
-from box import Box
+from box import Game, Floor
       
+def main():
+    root = Tk()
+    canvas = Canvas(root, width=600, height=600)
+    floor = PhotoImage(file="images/floor.png")
+    
+    game = Game()
 
-root = Tk()
-canvas = Canvas(root, width=600, height=600)
-
-floor = PhotoImage(file="images/floor.png")
-box = Box(0,0,floor)
-box2 = Box(0,61, floor)
+    for i in range(0,10):
+        tile = Floor(i*60,0, floor)
+        game.add_tiles(tile)
+        for j in range(1,10):
+            tile = Floor(i*60,j*60, floor)
+            game.add_tiles(tile)
+            print(tile)
+       
+    
+        
 
 # Create a function that can be called when a key pressing happens
-def on_key_press(e):
-    # When the keycode is 111 (up arrow) we move the position of our box higher
-    if e.keycode == 111:
-        box.testBoxY = box.testBoxY - 100
-    elif e.keycode == 116:
-        box.testBoxY = box.testBoxY + 100
+    def on_key_press(e):
+        # When the keycode is 111 (up arrow) we move the position of our box higher
+        if e.keycode == 111:
+            box.testBoxY = box.testBoxY - 100
+        elif e.keycode == 116:
+            box.testBoxY = box.testBoxY + 100
     # and lower if the key that was pressed the down arrow
     # draw the box again in the new position
-    box.draw(canvas)
+        box.draw(canvas)
 
 # Tell the canvas that we prepared a function that can deal with the key press events
-canvas.bind("<KeyPress>", on_key_press)
-canvas.pack()
+    canvas.bind("<KeyPress>", on_key_press)
+    canvas.pack()
 
 # Select the canvas to be in focused so it actually recieves the key hittings
-canvas.focus_set()
+    canvas.focus_set()
 
 # Draw the box in the initial position
-box.draw(canvas)
-box2.draw(canvas)
+    for tile in game.tiles:
+        game.draw(canvas)
+    
 
-root.mainloop()
+    root.mainloop()
 
+
+
+if __name__ == '__main__':
+    main()
